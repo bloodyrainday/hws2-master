6378bdca9eafc1f024cb73a7ef8f4eed3a631d2c
bloodyrainday <bloodyrainday@gmail.com>
2025-03-12 15:14:06 +0300

    make sort action work in homeWorkReducer in HW8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,20 +1,26 @@
-import {UserType} from '../HW8'
+import { UserType } from "../HW8";
 
 type ActionType =
-    | { type: 'sort'; payload: 'up' | 'down' }
-    | { type: 'check'; payload: number }
+  | { type: "sort"; payload: "up" | "down" }
+  | { type: "check"; payload: number };
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
-    switch (action.type) {
-        case 'sort': { // by name
-
-            return state // need to fix
-        }
-        case 'check': {
-
-            return state // need to fix
-        }
-        default:
-            return state
+export const homeWorkReducer = (
+  state: UserType[],
+  action: ActionType
+): UserType[] => {
+  // need to fix any
+  switch (action.type) {
+    case "sort": {
+      // by name
+      let copy = state;
+      return action.payload === "up"
+        ? copy.sort((a, b) => (a.name < b.name ? -1 : 1))
+        : copy.sort((a, b) => (a.name > b.name ? -1 : 1)); // need to fix
     }
-}
+    case "check": {
+      return state.filter((s) => s.age > action.payload); // need to fix
+    }
+    default:
+      return state;
+  }
+};
