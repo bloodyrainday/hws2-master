354adf9c9ec97f907ee06992107efaaefd0b9dbc
bloodyrainday <bloodyrainday@gmail.com>
2025-03-29 14:29:45 +0300

    fix loadingReducer

--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -1,22 +1,32 @@
-const initState = {
-    isLoading: false,
-}
+type LoadingType = {
+  isLoading: boolean;
+};
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
-    switch (action.type) {
-        // пишет студент  // need to fix
+const initState: LoadingType = {
+  isLoading: false,
+};
 
-        default:
-            return state
-    }
-}
+export const loadingReducer = (
+  state = initState,
+  action: LoadingActionType
+): LoadingType => {
+  debugger;
+  // fix any
+  switch (action.type) {
+    case "CHANGE_LOADING":
+      // пишет студент  // need to fix
+      return { ...state, isLoading: action.isLoading };
+    default:
+      return state;
+  }
+};
 
 type LoadingActionType = {
-    type: 'CHANGE_LOADING'
-    isLoading: boolean
-}
+  type: "CHANGE_LOADING";
+  isLoading: boolean;
+};
 
 export const loadingAC = (isLoading: boolean): LoadingActionType => ({
-    type: 'CHANGE_LOADING',
-    isLoading,
-})
+  type: "CHANGE_LOADING",
+  isLoading,
+});
