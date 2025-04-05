9b5d17bf5422bf0d50bd5bbe152498662f5025ec
bloodyrainday <bloodyrainday@gmail.com>
2025-04-05 17:31:24 +0300

    add change funciton

--- src/s2-homeworks/hw11/HW11.tsx
+++ src/s2-homeworks/hw11/HW11.tsx
@@ -21,7 +21,12 @@ function HW11() {
   const change = (event: Event, value: number | number[]) => {
     // пишет студент // если пришёл массив - сохранить значения в оба useState, иначе в первый
     console.log("change", event, value);
-    if (!Array.isArray(value)) setValue1(value);
+    if (!Array.isArray(value)) {
+      setValue1(value);
+    } else {
+      setValue1(value[0]);
+      setValue2(value[1]);
+    }
   };
 
   return (
@@ -47,8 +52,8 @@ function HW11() {
             </span>
             <SuperRange
               id={"hw11-double-slider"}
-              value={value2}
-
+              value={[value1, value2]}
+              onChange={change}
               // сделать так чтоб value1/2 изменялось // пишет студент
             />
             <span id={"hw11-value-2"} className={s.number}>
