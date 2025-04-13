55928e2588a5e41687e08c77354988e6a8458029
bloodyrainday <bloodyrainday@gmail.com>
2025-04-13 12:18:59 +0300

    add types and switch case of changeThemeId action in the themeReducer

--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -1,14 +1,28 @@
+type initStateType = {
+  themeId: number;
+};
+
 const initState = {
-    themeId: 1,
-}
+  themeId: 1,
+};
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
-    switch (action.type) {
-        // дописать
+export const themeReducer = (
+  state = initState,
+  action: changeThemeIdActionType
+): initStateType => {
+  // fix any
+  switch (action.type) {
+    // дописать
+    case "SET_THEME_ID":
+      return { ...state, themeId: action.id };
 
-        default:
-            return state
-    }
-}
+    default:
+      return state;
+  }
+};
 
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+export const changeThemeId = (
+  id: number
+): { type: "SET_THEME_ID"; id: number } => ({ type: "SET_THEME_ID", id }); // fix any
+
+type changeThemeIdActionType = ReturnType<typeof changeThemeId>;
